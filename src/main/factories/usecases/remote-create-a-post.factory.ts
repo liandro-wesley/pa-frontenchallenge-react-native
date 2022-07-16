a4ecbb077678b5e45a4b4d3e9f00832d2826abec
Liandro Silva <liandro.silva1012@gmail.com>
2022-07-15 23:35:19 -0300

    feat: add factory to call CreateAPost

---
+++ src/main/factories/usecases/remote-create-a-post.factory.ts
@@ -0,0 +1,8 @@
+import {RemoteCreateAPost} from '@data/usecases/remote-create-a-post.data';
+import {CreateAPost} from '@domain/usecases/create-a-post.domain';
+import {makeApiUrl} from '../http/api-url.factory';
+import {makeHttpClient} from '../http/http-client.factory';
+
+export const makeCreateAPost = (): CreateAPost => {
+  return new RemoteCreateAPost(makeApiUrl('/posts'), makeHttpClient());
+};
