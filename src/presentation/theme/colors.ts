229fadb50efc16e1a7b341889a6de55a5d40522e
Liandro Silva <liandro.silva1012@gmail.com>
2022-07-16 22:21:00 -0300

    style: add new design tokens for theme

--- src/presentation/theme/colors.ts
+++ src/presentation/theme/colors.ts
@@ -1,5 +1,12 @@
 export default {
   colors: {
-    black900: '#000',
+    black900: '#000000',
+    black800: '#171717',
+    black700: '#262626',
+
+    white900: '#FFFFFF',
+    white800: '#F2F2F2',
+
+    accentColor: '#4F46E5',
   },
 };
---
+++ src/presentation/theme/shadow.ts
@@ -0,0 +1,5 @@
+export default {
+  shadow: {
+    normal: '0 0 0 1px rgba(255,255,255, 0.15)',
+  },
+};
