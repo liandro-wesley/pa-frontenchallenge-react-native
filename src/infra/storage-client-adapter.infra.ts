fb6ee412d93eb169b78acc7c748ad83a06d4d29e
Liandro Silva <liandro.silva1012@gmail.com>
2022-07-18 17:45:01 -0300

    docs: add jsdoc in StorageClientAdapter

--- src/infra/storage-client-adapter.infra.ts
+++ src/infra/storage-client-adapter.infra.ts
@@ -4,15 +4,28 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export class StorageClientAdapter
   implements SetStorage, GetStorage, RemoveStorage
 {
+  /**
+   * @async
+   * @param key Reference to get a value in storage
+   */
   public async get(key: string): Promise<any> {
     const data = await AsyncStorage.getItem(key);
     return JSON.parse(`${data}`);
   }
 
+  /**
+   * @async
+   * @param key Reference to set a value in storage
+   * @param value An object you want to insert into storage
+   */
   public async set(key: string, value: object): Promise<void> {
     return await AsyncStorage.setItem(key, JSON.stringify(value));
   }
 
+  /**
+   * @async
+   * @param key Reference to remove a value in storage
+   */
   public async remove(key: string): Promise<void> {
     return await AsyncStorage.removeItem(key);
   }
