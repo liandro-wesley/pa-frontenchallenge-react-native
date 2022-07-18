2e9f4c42355b85553a1b145619fb6ed470f17500
Liandro Silva <liandro.silva1012@gmail.com>
2022-07-18 16:49:53 -0300

    styles: add padding in WrapperLayout

--- src/presentation/layouts/WrapperLayout/style.ts
+++ src/presentation/layouts/WrapperLayout/style.ts
@@ -3,7 +3,7 @@ import styled, {css} from 'styled-components/native';
 export const WrapperLayout = styled.View`
   ${({theme}) => css`
     background-color: ${theme.colors.black900};
-    padding: ${theme.spacing.normal};
+    padding: ${theme.spacing.large};
     flex: 1;
   `}
 `;
