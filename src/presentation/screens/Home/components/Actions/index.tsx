0e1858f87512b1de08440839441e16045491d6a1
Liandro Silva <liandro.silva1012@gmail.com>
2022-07-21 11:17:59 -0300

    feat: ensure redirect to FavoritePostsScreen when user click in FavoriteIcon

--- src/presentation/screens/Home/components/Actions/index.tsx
+++ src/presentation/screens/Home/components/Actions/index.tsx
@@ -4,15 +4,18 @@ import * as S from './styles';
 import PostFind from '@presentation/screens/Home/components/PostFind';
 import Typography from '@presentation/components/Typography';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {useHomeScreenContext} from '../../contex';
+import {Routes} from '@main/navigation/routes';
 
 const FavoriteIcon = memo(() => <Icon name="heart-o" size={24} />);
 
 const Action: React.FC = () => {
+  const {navigation} = useHomeScreenContext();
   return (
     <S.Container>
       <PostFind />
       <S.PostActions>
-        <S.PostActionsItem>
+        <S.PostActionsItem onPress={() => navigation.navigate(Routes.FAVORITE)}>
           <Typography variant="body" color="white800">
             <FavoriteIcon />
           </Typography>
